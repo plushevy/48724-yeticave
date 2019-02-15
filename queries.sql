df8553ad0559e7d24e25a534b4a8a56968d60009
Molchanov Ilia <plue@inbox.ru>
2019-02-15 16:38:53 +0300

    module4-task2 | Правки БД, запросов, полнотекстовый поиск

--- queries.sql
+++ queries.sql
@@ -25,15 +25,15 @@ VALUES
   ( '2019-01-24 14:10:59', '2019-03-16 23:59:59', 'Маска Oakley Canopy', 'Описание6', 'img/lot-6.jpg', 5400, 300, 3, 6);
 
 
-INSERT INTO bets (dt_create, rate, id_user, id_lot)
+INSERT INTO bets (dt_create, last_price, id_user, id_lot)
 VALUES
-  ('2019-01-27 14:54:59', 11500, 1, 1 ),
-  ('2019-01-28 14:54:59', 11000, 1, 2 ),
-  ('2019-02-01 14:54:59', 10500, 1, 3 ),
-  ('2019-02-05 14:54:59', 18300, 2, 1 ),
-  ('2019-02-08 14:54:59', 12500, 2, 2 ),
-  ('2019-02-10 14:54:59', 15500, 3, 4 ),
-  ('2019-01-14 14:54:59', 19000, 3, 6 );
+  ('2019-01-27 14:54:59', 21500, 1, 1 ),
+  ('2019-01-28 14:54:59', 165999, 1, 2 ),
+  ('2019-02-01 14:54:59', 9500, 1, 3 ),
+  ('2019-02-05 14:54:59', 23300, 2, 1 ),
+  ('2019-02-08 14:54:59', 183999, 2, 2 ),
+  ('2019-02-10 14:54:59', 16300, 3, 4 ),
+  ('2019-01-14 14:54:59', 7400, 3, 6 );
 
 
 -- получить все категории;
@@ -41,19 +41,19 @@ SELECT name FROM categories;
 
 
 -- получить самые новые, открытые лоты. Каждый лот должен включать название, стартовую цену, ссылку на изображение, цену, название категории;
-SELECT  label, start_price, img_url, max(b.rate) + start_price as last_price , c.name
+SELECT  l.label, l.start_price, l.img_url, IFNULL(max(b.last_price), l.start_price) , c.name
 FROM lots l
-JOIN bets b ON b.id_lot = l.id
-JOIN categories c ON c.id = l.id_category
-WHERE dt_end > CURDATE()
-GROUP BY label
-ORDER BY dt_add DESC;
+  LEFT JOIN bets b ON b.id_lot = l.id
+  JOIN categories c ON c.id = l.id_category
+WHERE l.dt_end > CURDATE()
+GROUP BY l.id
+ORDER BY l.dt_add DESC;
 
 
 -- показать лот по его id. Получите также название категории, к которой принадлежит лот
-SELECT name, description, start_price, c.name as category
+SELECT l.name, l.description, l.start_price, c.name as category
 FROM lots l
-JOIN categories c ON c.id = l.id_category
+  JOIN categories c ON c.id = l.id_category
 WHERE l.id = 2;
 
 
@@ -62,13 +62,18 @@ UPDATE lots SET label = 'Новое название' WHERE id = 3;
 
 
 -- получить список самых свежих ставок для лота по его идентификатору (дата ставки, ставка, кто поставил)
-SELECT dt_create, rate, u.name
+SELECT b.dt_create, b.last_price, u.name
 FROM bets b
-JOIN lots l ON b.id_lot = l.id
-JOIN users u ON b.id_user = u.id
+  JOIN lots l ON b.id_lot = l.id
+  JOIN users u ON b.id_user = u.id
 WHERE l.id = 2
-ORDER BY dt_create DESC
+ORDER BY b.dt_create DESC
 LIMIT 5;
 
---найти лот по его названию или описанию;
+--найти лот по его названию или описанию НЕОБЯЗАТЕЛЬНО;
 SELECT * FROM lots l WHERE l.label LIKE 'бот%' OR l.description LIKE 'бот%';
+
+-- Выполнить поиск в таблице лотов по полям «имя» и «описание», используя полнотекстовый поиск (операторы «MATCH ... AGAINST»)
+SELECT * FROM lots
+WHERE
+  MATCH (label, description) AGAINST('*Бот*' IN BOOLEAN MODE);
--- schema.sql
+++ schema.sql
@@ -33,28 +33,97 @@ CREATE TABLE lots(
   bet_step INT,
   id_user INT,
   id_winner INT,
-  id_category INT
+  id_category INT,
+  FULLTEXT (`label`, `description`)
 );
 
 
 CREATE TABLE bets(
   id INT PRIMARY KEY AUTO_INCREMENT,
   dt_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  rate INT,
+  last_price INT,
   id_user INT,
   id_lot INT
 );
 
 
-CREATE INDEX u_email_id_index ON users(email);
-CREATE INDEX b_rate_id_index ON bets(rate);
+CREATE INDEX bets_id_lot_last_price ON bets(id_lot, last_price);
 CREATE INDEX l_dt_add_id_index ON lots(dt_add);
 CREATE INDEX l_dt_end_id_index ON lots(dt_end);
--- CREATE INDEX l_label_id_index ON lots(label);
--- CREATE INDEX l_category_id_index ON lots(id_category);
--- CREATE INDEX b_dt_create_id_index ON bets(dt_create);
 
 
+-- FOREING KEYS
+ALTER TABLE bets
+  ADD CONSTRAINT bets_user_id__fk
+FOREIGN KEY (id_user) REFERENCES users (id);
+
+ALTER TABLE bets
+  ADD CONSTRAINT bets_lot_id__fk
+FOREIGN KEY (id_lot) REFERENCES lots (id);
+
+ALTER TABLE lots
+  ADD CONSTRAINT lots_user_id__fk
+FOREIGN KEY (id_user) REFERENCES users (id);
+
+ALTER TABLE lots
+  ADD CONSTRAINT lots_winner_id__fk
+FOREIGN KEY (id_winner) REFERENCES users (id);
+
+ALTER TABLE lots
+  ADD CONSTRAINT lots_category_id__fk
+FOREIGN KEY (id_category) REFERENCES categories (id);
+
+CREATE DATABASE yeticave_db
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave_db;
+
+
+CREATE TABLE categories(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(32) UNIQUE
+);
+
+
+CREATE TABLE users(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  dt_registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email VARCHAR(128) UNIQUE NOT NULL,
+  name VARCHAR(64) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  img_url VARCHAR(128),
+  contacts VARCHAR(128)
+);
+
+CREATE TABLE lots(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  dt_end TIMESTAMP,
+  label VARCHAR(128) NOT NULL,
+  description TEXT,
+  img_url VARCHAR(128),
+  start_price INT,
+  bet_step INT,
+  id_user INT,
+  id_winner INT,
+  id_category INT,
+  FULLTEXT (`label`, `description`)
+);
+
+
+CREATE TABLE bets(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  dt_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  last_price INT,
+  id_user INT,
+  id_lot INT
+);
+
+
+CREATE INDEX bets_id_lot_last_price ON bets(id_lot, last_price);
+CREATE INDEX l_dt_add_id_index ON lots(dt_add);
+CREATE INDEX l_dt_end_id_index ON lots(dt_end);
 
 
 -- FOREING KEYS
